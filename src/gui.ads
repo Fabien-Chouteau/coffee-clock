bb298b64c46005b7bf81c08fe26b25df083920ec
Fabien Chouteau <fabien.chouteau@gmail.com>
2017-04-22 18:31:44 +0200

    Add headers

--- src/gui.ads
+++ src/gui.ads
@@ -1,3 +1,24 @@
+-------------------------------------------------------------------------------
+--                                                                           --
+--                               Coffee Clock                                --
+--                                                                           --
+--                  Copyright (C) 2016-2017 Fabien Chouteau                  --
+--                                                                           --
+--    Coffee Clock is free software: you can redistribute it and/or          --
+--    modify it under the terms of the GNU General Public License as         --
+--    published by the Free Software Foundation, either version 3 of the     --
+--    License, or (at your option) any later version.                        --
+--                                                                           --
+--    Coffee Clock is distributed in the hope that it will be useful,        --
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of         --
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       --
+--    General Public License for more details.                               --
+--                                                                           --
+--    You should have received a copy of the GNU General Public License      --
+--    along with We Noise Maker. If not, see <http://www.gnu.org/licenses/>. --
+--                                                                           --
+-------------------------------------------------------------------------------
+
 with Interfaces; use Interfaces;
 
 package GUI is
